7860bd976c00ddb7d12e7402d1ea0c372571511c
iDevo <idevo.eleazar@gmail.com>
2026-01-21 23:42:08 +0000

    Delete src directory

--- src/commands/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod install;
-pub mod uninstall;
-pub mod run;
-pub mod start;
-pub mod init;
--- src/core/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod cas;
-pub mod resolver;
-pub mod registry;
-pub mod extractor;
-pub mod installer;
-pub mod config;
