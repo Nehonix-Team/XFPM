a0967d7776c3e82a155b7c8b112bcea01ec2004c
iDevo <idevo.eleazar@gmail.com>
2026-03-01 15:11:06 +0000

    chore: update xfpm-go subproject to latest commit (d973de2e)

--- cmd/xfpm/run.go
+++ cmd/xfpm/run.go
@@ -1,3 +1,9 @@
+/***************************************************************************
+ * XFPM (XyPriss Fast Package Manager)
+ * @license Nehonix OSL (NOSL)
+ * Copyright (c) 2025 Nehonix. All rights reserved.
+ ***************************************************************************** */
+
 package main
 
 import (
