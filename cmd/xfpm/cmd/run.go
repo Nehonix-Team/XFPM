8836f2f4f670bfabcc6f8df7d021426a0be5b33a
iDevo <idevo.eleazar@gmail.com>
2026-09-20 15:57:17 +0000

    feat(libxess): pass XyPriss temp directory to libXESS supervisor

--- cmd/xfpm/cmd/run.go
+++ cmd/xfpm/cmd/run.go
@@ -99,11 +99,15 @@ func executeShell(command, dir string) error {
 			shellCmd = []string{"sh", "-c", command}
 		}
 
+		xyTempDir := filepath.Join(os.TempDir(), "nehonix.xypriss.data", "xess")
+		_ = os.MkdirAll(xyTempDir, 0700)
+
 		sup, err := libxess.NewSupervisor(libxess.Config{
 			ProjectDir:  dir,
 			EnvFileName: ".env",
 			Command:     shellCmd,
 			BaseEnv:     baseEnv,
+			TempDir:     xyTempDir,
 		})
 		if err == nil {
 			cmd, err := sup.Start()
@@ -134,11 +138,15 @@ func executeCommand(name string, args []string, dir string) error {
 
 	if _, err := os.Stat(envPath); err == nil {
 		fullCmd := append([]string{name}, args...)
+		xyTempDir := filepath.Join(os.TempDir(), "nehonix.xypriss.data", "xess")
+		_ = os.MkdirAll(xyTempDir, 0700)
+
 		sup, err := libxess.NewSupervisor(libxess.Config{
 			ProjectDir:  dir,
 			EnvFileName: ".env",
 			Command:     fullCmd,
 			BaseEnv:     baseEnv,
+			TempDir:     xyTempDir,
 		})
 		if err == nil {
 			cmd, err := sup.Start()
